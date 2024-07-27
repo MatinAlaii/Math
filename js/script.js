7d3df33bfd8cce38be37e59ae53207b6e71ef07f
MatinAlaii <matinalaii44@gmail.com>
2024-07-27 16:27:15 +0330

    Math commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+document.getElementById("demo1").innerHTML = Math.round(10.5);
+
+document.getElementById("demo2").innerHTML = Math.ceil(12.2);
+
+document.getElementById("demo3").innerHTML = Math.floor(13.6);
+
+document.getElementById("demo4").innerHTML = Math.trunc(9.7);
+
+document.getElementById("demo5").innerHTML = Math.sign(6);
+
+document.getElementById("demo6").innerHTML = Math.pow(8, 2);
+
+document.getElementById("demo7").innerHTML = Math.sqrt(64);
+
+document.getElementById("demo8").innerHTML = Math.abs(-4.7);
+
+document.getElementById("demo9").innerHTML =
+  "The sine value of 90 degrees is " + Math.sin((90 * Math.PI) / 180);
+
+document.getElementById("demo10").innerHTML =
+  "The cosine value of 0 degrees is " + Math.cos((0 * Math.PI) / 180);
+
+document.getElementById("demo11").innerHTML = Math.min(
+  0,
+  150,
+  30,
+  20,
+  -8,
+  -200
+);
+
+document.getElementById("demo12").innerHTML = Math.max(
+  0,
+  150,
+  30,
+  20,
+  -8,
+  -200
+);
+
+document.getElementById("demo13").innerHTML = Math.random();
+
+document.getElementById("demo14").innerHTML = Math.log(4);
+
+document.getElementById("demo15").innerHTML = Math.log(10);
+
+document.getElementById("demo16").innerHTML = Math.log2(8);
+
+document.getElementById("demo17").innerHTML = Math.log10(1000);
